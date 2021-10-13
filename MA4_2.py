82f65b63bd1f98f7903875c3ee39eefb9f093b59
sarasvanberg <92375564+sarasvanberg@users.noreply.github.com>
2021-10-13 09:14:47 +0200

    MA4.3

--- MA4_2.py
+++ MA4_2.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
 from integer import Integer
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
 
 def main():
 	f = Integer(5)
 	print(f.get())
 	f.set(7)
 	print(f.get())
+	plt.plot([1,2,3],[1,2,3]) # do your plotting here
+	plt.savefig("fibonacci_timing.png") 
 
 if __name__ == '__main__':
 	main()
